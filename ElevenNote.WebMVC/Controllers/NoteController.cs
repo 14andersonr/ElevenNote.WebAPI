54fe3322f301232e6cf5326f01d4d934af9e5d6f
Richard Anderson III <14andersonr@gmail.com>
2021-03-27 14:51:31 -0400

    Added Create Methods in MVC

--- ElevenNote.WebMVC/Controllers/NoteController.cs
+++ ElevenNote.WebMVC/Controllers/NoteController.cs
@@ -1,9 +1,12 @@
 ﻿using ElevenNote.WebAPI.Models;
+using ElevenNote.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using ElevenNote.Services;
+using Microsoft.AspNet.Identity;
 
 namespace ElevenNote.WebMVC.Controllers
 {
@@ -13,8 +16,36 @@ namespace ElevenNote.WebMVC.Controllers
         // GET: Note
         public ActionResult Index()
         {
-            var model = new NoteListItem[0];
+            var userId = Guid.Parse(User.Identity.GetUserId());
+            var service = new NoteService(userId);
+            var model = service.GetNotes();
+
             return View(model);
+        }
+
+        //GET: Note/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+
+        //POST: Note/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(NoteCreate model)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(model);
+            }
+
+            var userId = Guid.Parse(User.Identity.GetUserId());
+            var service = new NoteService(userId);
+
+            service.CreateNote(model);
+
+            return RedirectToAction("Index");
         }
     }
 }
